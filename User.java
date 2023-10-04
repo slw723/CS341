52fd7d04ec880c38439b7a348197f5156ea00632
Sidney Williams <101272072+slw723@users.noreply.github.com>
2023-10-03 19:28:43 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,59 @@
-public class User {
-    
+public class User extends Appointment {
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @Email
+    @NotEmpty
+    private String email;
+    private String password;
+    private int phoneNumber;
+
+    public User (String first, String last, String emailAddress, String pw, int phone) {
+        firstName = first;
+        lastName = last;
+        email = emailAddress;
+        password = pw;
+        phoneNumber = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String first) {
+        firstName = first;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String last) {
+        lastName = last;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String emailAddress) {
+        email = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String pw) {
+        password = pw;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phone) {
+        phoneNumber = phone;
+    }
 }
