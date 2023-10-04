1b24b521c67feb6c6ad63abf46fc952a52c880c2
Sidney Williams <101272072+slw723@users.noreply.github.com>
2023-10-03 19:26:00 -0500

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,3 +1,17 @@
 public class Admin {
-    
+    @Id
+    private String userId;
+
+    @NotEmpty
+    private String password;
+
+
+    public Admin(String id, String pw){
+        userId = id;
+        password = pw;
+    }
+
+    public Admin() { //empty constructor
+
+    }
 }
